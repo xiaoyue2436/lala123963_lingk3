464e2815eb2f0226c22a0801535a062263082cf3
xiaoyue2436 <108921969+xiaoyue2436@users.noreply.github.com>
2023-07-25 16:08:45 +0800

    Update 星空免费小说.js

--- 星空免费小说.js
+++ 星空免费小说.js
@@ -7,7 +7,8 @@
  * 项目抓包：抓api-gw.zonghengxiaoshuo.com下的authorization&qm-params填入变量
  * 项目变量：lekebo_xkxs_Cookie
  * 项目定时：每30分钟运行一次
- * 定时规则: * 30 * * * *
+ * 定时规则: 
+ * cron:10 4,8,12,16,20,0 * * *
  * 
  * 邀 请 码: 
  * 
